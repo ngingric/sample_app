bc3d20f3724c6d89cda8b5fea4e399c045b33699
Nathanael Gingrich <ngingric@gmail.com>
2012-12-13 18:29:53 -0600

    Chapter 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,7 +20,7 @@ describe "User pages" do
   end
   
   describe "profile page" do
-    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
   
     it { should have_selector('h1',    text: user.name) }
@@ -37,6 +37,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -50,6 +57,21 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+      
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
+    
   end
 end
