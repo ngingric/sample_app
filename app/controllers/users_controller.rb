9c6cd92bfeaab3470908db2bd6e405a481bbb326
Nathanael Gingrich <ngingric@gmail.com>
2013-01-04 18:25:21 -0600

    Moved the rails app to the root and removed the backend directory

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json  { render :json => @user }
+      format.xml  { render :xml => @user }
+    end
   end
 
   def new
